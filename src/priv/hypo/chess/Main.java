46087a9507316d772848c19d4c2c8cede9c1048b
zhaibo <hypozng@qq.com>
2019-07-02 17:27:15 +0800

    添加相关注释

--- src/priv/hypo/chess/Main.java
+++ src/priv/hypo/chess/Main.java
@@ -10,8 +10,13 @@ import priv.hypo.chess.frame.ChessFrame;
 public class Main {
 
 	public static void main(String[] args) throws Exception {
+        // 创建主窗体实例
 		ChessFrame frame = new ChessFrame();
+
+        // 设置主窗体关闭退出程序的操作
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        // 显示主窗体
 		frame.setVisible(true);
 	}
 
